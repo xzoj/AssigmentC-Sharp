800afa1e17f3b0d398d3e000c16c92322871497d
AnhNT <31509113+xzoj@users.noreply.github.com>
2018-06-26 19:39:28 +0700

    NTA

---
+++ Assigment/Assigment/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace Assigment
+{
+    public class Product
+    {
+        private string productId;
+        private string productName;
+        private int price;
+
+        public Product()
+        {
+        }
+
+        public Product(string productId, string productName, int price)
+        {
+            this.productId = productId;
+            this.productName = productName;
+            this.price = price;
+        }
+
+        public string ProductId
+        {
+            get => productId;
+            set => productId = value;
+        }
+
+        public string ProductName
+        {
+            get => productName;
+            set => productName = value;
+        }
+
+        public int Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}
---
+++ Assigment/Assigment/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+namespace Assigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            
+            while (true)
+            {
+                Console.WriteLine("--------Menu------");
+                Console.WriteLine("1: Add a product.");
+                Console.WriteLine("2: Display product.");
+                Console.WriteLine("3: Delete product.");
+                Console.WriteLine("4: Exit.");
+                Console.WriteLine("------------------");
+                Console.WriteLine("Enter your choice!");
+                var choice = Int32.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                    {
+                        list.Add(Add());
+                        break;
+                    }
+                    case 2:
+                    {
+                        Display(list);
+                        break;
+                    }
+                    case 3:
+                    {
+                        Delete(list);
+                        break;
+                    }
+                    case 4:
+                    {
+                        Console.WriteLine("Goodbye!!");
+                        System.Environment.Exit(1);
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Please try again.");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Delete(List<Product> list)
+        {
+            
+            Console.WriteLine("Enter Product ID to DELETE.");
+            string producid = Console.ReadLine();
+            var itemToRemove = list.SingleOrDefault(r => r.ProductId == producid);
+            if (itemToRemove != null)
+            {
+                list.Remove(itemToRemove);
+                Console.WriteLine("Delete done.");
+            }
+                            
+        }
+
+        private static void Display(List<Product> list)
+        {
+            Console.WriteLine("Product ID"+"    "+"Product Name"+"      "+"Price");
+            Console.WriteLine("=================================================");
+            foreach (var product in list)
+            {
+                Console.WriteLine(product.ProductId +"  "+ product.ProductName+"  "+ product.Price);
+            }
+        }
+
+        private static Product Add()
+        {
+            Console.WriteLine("Enter product ID:");
+            string productId = Console.ReadLine();
+            Console.WriteLine("Enter product Name:");
+            string productName = Console.ReadLine();
+            Console.WriteLine("Enter product Price:");
+            int price = Console.Read();
+            return new Product(productId, productName, price);
+        }
+    }
+}
